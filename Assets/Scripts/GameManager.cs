3747ec17ce7c584bd60214c411144ba2fab970ec
tazzan <tazminkiwi@gmail.com>
2025-11-03 14:44:01 -0500

    New enemy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
 
     public GameObject enemyOnePrefab;
+    public GameObject enemyTwoPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1, 2);
+        InvokeRepeating("CreateEnemyTwo", 1, 1);
     }
 
     // Update is called once per frame
@@ -22,5 +24,10 @@ public class GameManager : MonoBehaviour
     void CreateEnemyOne()
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
     }
 }
